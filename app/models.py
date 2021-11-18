8cd1bffd59b9290789cc465e004f9eb5e5bcee43
KOLAWOLE OGUNFOWOKAN <kolawole.ogunfowokan@konga.com>
2021-11-18 13:52:28 +0100

    ensure only a user can update or delete a post

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 class Post(Base):
    __tablename__ = "posts"
@@ -11,10 +11,11 @@ class Post(Base):
    content = Column(String,nullable=False)
    published = Column(Boolean, server_default="TRUE", nullable=False)
    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+   owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class User(Base):
-   __tablename__ = "users"
+   __tablename__ = "users" 
    
    id = Column(Integer, primary_key=True, nullable=False)
    email = Column(String, nullable=False, unique=True)
--- app/routers/post.py
+++ app/routers/post.py
@@ -17,7 +17,7 @@ async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(o
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-   new_post = models.Post(**post.dict())
+   new_post = models.Post(owner_id=current_user.id, **post.dict())
    db.add(new_post)
    db.commit()
    db.refresh(new_post)
@@ -31,13 +31,15 @@ async def get_post(id: int, db: Session = Depends(get_db), current_user: int = D
    return post
 
 @router.put('/{id}', response_model=schemas.Post)
-async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
    
    post_query = db.query(models.Post).filter(models.Post.id == id)
    updated_post = post_query.first()
 
    if updated_post == None:
       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} not found')
+   if post.owner_id != current_user.id:
+      raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f'not authorized to perform requested action')
    post_query.update(post.dict(), synchronize_session=False)
    db.commit()
    return post_query.first()
@@ -45,11 +47,14 @@ async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(g
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-   post = db.query(models.Post).filter(models.Post.id == id)
+   post_query = db.query(models.Post).filter(models.Post.id == id)
+   post = post_query.first()
 
-   if post.first() == None:
+   if post == None:
       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} not found')
-   post.delete(synchronize_session=False)
+   if post.owner_id != current_user.id:
+      raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f'not authorized to perform requested action')
+   post_query.delete(synchronize_session=False)
    db.commit()
    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
