bf1efbcb7d76559a84015e4e5dc45002898f523b
KOLAWOLE OGUNFOWOKAN <kolawole.ogunfowokan@konga.com>
2021-11-21 20:33:05 +0100

    configure environment variables, class based

--- app/database.py
+++ app/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#import psycopg2
-#from psycopg2.extras import RealDictCursor
-#import time
+from . config import settings
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345678@localhost/fastapi-python'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -20,16 +18,3 @@ def get_db():
    finally:
       db.close()
 
-###This is just for record purpose, we are actually connect to the db via sqlalchemy.
-### If we connect this way, we can use raw sql with postgresql
-# while True:
-
-#    try:
-#       conn = psycopg2.connect(host='localhost', database='fastapi-python', user='postgres', password='12345678', cursor_factory=RealDictCursor)
-#       cursor = conn.cursor()
-#       print("Database connection was succesfull")
-#       break
-#    except Exception as error:
-#       print("Connection to database failed")
-#       print("Error: ", error)
-#       time.sleep(2)
