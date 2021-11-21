5d8663563e05c53cb767eaae226bc860f0bbcd17
KOLAWOLE OGUNFOWOKAN <kolawole.ogunfowokan@konga.com>
2021-11-21 08:19:39 +0100

    implement query parameters

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+#import psycopg2
+#from psycopg2.extras import RealDictCursor
+#import time
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345678@localhost/fastapi-python'
 
@@ -16,3 +19,17 @@ def get_db():
       yield db
    finally:
       db.close()
+
+###This is just for record purpose, we are actually connect to the db via sqlalchemy.
+### If we connect this way, we can use raw sql with postgresql
+# while True:
+
+#    try:
+#       conn = psycopg2.connect(host='localhost', database='fastapi-python', user='postgres', password='12345678', cursor_factory=RealDictCursor)
+#       cursor = conn.cursor()
+#       print("Database connection was succesfull")
+#       break
+#    except Exception as error:
+#       print("Connection to database failed")
+#       print("Error: ", error)
+#       time.sleep(2)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from typing import  List
+from typing import  List, Optional
 from fastapi import  Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import models, schemas, oauth2
@@ -10,10 +10,18 @@ router = APIRouter(
    tags=['Posts']
 )
 
-@router.get('/', response_model=List[schemas.Post])
-async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-   posts = db.query(models.Post).all()
+@router.get('/admin', response_model=List[schemas.Post])
+async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ''):
+   posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
    return posts
+
+
+@router.get('/user', response_model=List[schemas.Post])
+async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),limit: int = 10, skip: int = 0, search: Optional[str]= ''):
+   posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+   return posts
+
+
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
@@ -23,11 +31,14 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), c
    db.refresh(new_post)
    return  new_post
 
+
 @router.get('/{id}', response_model=schemas.Post)
 async def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
    post = db.query(models.Post).filter(models.Post.id == id).first()
    if not post:
       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} not found')
+   if post.owner_id != current_user.id:
+      raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f'not authorized to perform requested action')
    return post
 
 @router.put('/{id}', response_model=schemas.Post)
